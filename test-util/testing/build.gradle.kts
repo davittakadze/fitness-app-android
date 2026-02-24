7337a1d689c8f8f64a923cfef64abdd572c6dadf
davittakadze <davit.takadze.1@btu.edu.ge>
2026-02-24 20:51:38 +0400

    add test-util for base unit tests

---
+++ test-util/testing/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java-library")
+    alias(libs.plugins.kotlin.jvm)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+    }
+}
+
+dependencies {
+    api(libs.junit)
+    api(libs.mockk)
+    api(libs.kotlinx.coroutines.test)
+    api(libs.turbine)
+}
---
+++ test-util/testing/src/main/java/com/example/testing/BaseUnitTest.kt
@@ -0,0 +1,25 @@
+package com.example.testing
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+
+@ExperimentalCoroutinesApi
+abstract class BaseUnitTest {
+    protected val testDispatcher: TestDispatcher = StandardTestDispatcher()
+
+    @Before
+    open fun setUp() {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    @After
+    open fun tearDown() {
+        Dispatchers.resetMain()
+    }
+}
